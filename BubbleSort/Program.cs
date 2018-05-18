a3d9acdd6d7f16eb1ddb4793201bea7534b02fba
Saulo Carvalho <saulo.carv@hotmail.com>
2018-05-18 20:55:33 -0300

    BubbleSort Simples Completo

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -6,7 +6,27 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vetor = new int[] { 10, 0, 11, -1, 201, 13, 123 };
+
+
+            for(int rodada=0; rodada<vetor.Length; rodada++)
+            {
+                for(int atual = 0; atual < vetor.Length-1; atual++)
+                {
+                    if(vetor[atual] > vetor[atual +1])
+                    {
+                        int troca = vetor[atual];
+                        vetor[atual] = vetor[atual + 1];
+                        vetor[atual + 1] = troca;
+                    }
+                }
+            }
+
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+            Console.ReadKey();
         }
     }
 }

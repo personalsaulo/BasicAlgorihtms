7ce489d5ef24a00b75561f978929cfaa59902f5a
Saulo Carvalho <saulo.carv@hotmail.com>
2018-05-18 21:47:09 -0300

    Inserção de campo minado

---
+++ BattleShip/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BattleShip.Global" Language="C#" %>
---
+++ CampoMInado/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CampoMInado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+
+            const int qtdBombas = 10;
+            const int quantidadeJogadas = 10;
+            char[,] campo = new char[n, n];
+
+            Random mistura = new Random();
+            for (int i = 0; i < qtdBombas; i++)
+            {
+                int linha = mistura.Next(0, n - 1);
+                int coluna = mistura.Next(0, n - 1);
+
+                campo[linha, coluna] = 'B';
+
+            }
+        }
+    }
+}
